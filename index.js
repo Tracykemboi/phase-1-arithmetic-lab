ccb6bc03624581b9998275f35410f007b62da946
Tracy Kemboi <tkemboi72@gmail.com>
2024-06-29 00:59:33 +0300

    Arithmetic lab js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let num1=62;
+let num2=1;
+multiply=num1*num2;
+
+let random= Math.floor(Math.random()*3)+1;
+console.log(random);
+
+let num3=14;
+let num4=5;
+let mod=num3%num4;
+
+let max=Math.max(20,19,8,12);
+console.log(max);
